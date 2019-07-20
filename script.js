3e3f2a817d617aea9826cb59ca6b5c3145e61416
Jacob Bankston <jacob.bankston.smile@gmail.com>
2019-07-19 22:37:19 -0500

    updated css, finishing tomorrow.

--- script.js
+++ script.js
@@ -12,23 +12,28 @@ $(document).ready(() => {
         // Saving the title and priority values
         let taskTitle = taskName.val()
         taskName.val('')
+        console.log(taskName.val())
         let priorityTitle = priorityLevel.val()
         priorityLevel.val('')
+        console.log(priorityLevel.val())
 
         // Placing the values into containers
 
         // name of task
         let taskItemDivTitle = $('<div class="task-titles">')
         taskItemDivTitle.html(taskTitle)
+        console.log(taskItemDivTitle.html())
         // priority level
         let taskItemDivPriority = $('<div class="task-priorities">')
         taskItemDivPriority.html(priorityTitle)
+        console.log(taskItemDivPriority.html())
         // checkbox and remove button
         let rbutton = $('<button class="removeButton">Remove</button>')
         let cbox = $('<input class="checkedBox" type="checkbox" />')
         
         // Placing value into task container
-        taskItemDiv.html(taskItemDivTitle.html() + taskItemDivPriority.html()).append(rbutton).prepend(cbox)
+        taskItemDiv.append(taskItemDivTitle.html()).append(taskItemDivPriority.html()).append(rbutton).prepend(cbox)
+        console.log(taskItemDiv.text())
         // Placing into the main pending div
         pendingtasks.append(taskItemDiv)
 
